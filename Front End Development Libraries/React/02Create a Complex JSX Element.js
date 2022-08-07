4e94dcdc3adbbb49024cf36c3e56f5d70b579702
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-08-07 21:44:46 +0530

    02Create a Complex JSX Element.js

---
+++ Front End Development Libraries/React/02Create a Complex JSX Element.js
@@ -0,0 +1,11 @@
+const JSX = (
+  <div>
+    <h1>My React</h1>
+    <p>Explore some features</p>
+    <ul>
+      <li>props</li>
+      <li>state</li>
+      <li>components</li>
+    </ul>
+  </div>
+);
